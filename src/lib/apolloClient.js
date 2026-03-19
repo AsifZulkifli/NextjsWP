4e3865ca6c85536cb6754c893de21c75a9166492
Afiq-DS <afiq.safri@digitalsymphony.it>
2026-03-19 15:38:59 +0800

    update CORS

--- src/lib/apolloClient.js
+++ src/lib/apolloClient.js
@@ -3,6 +3,9 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 const client = new ApolloClient({
   link: new HttpLink({
     uri: process.env.NEXT_PUBLIC_WORDPRESS_GRAPHQL_URL,
+    headers: {
+      "User-Agent": "DigitalSymphonyNextApp/1.0",
+    },
   }),
   cache: new InMemoryCache(),
 });
