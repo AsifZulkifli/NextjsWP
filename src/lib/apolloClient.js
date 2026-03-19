4185d5e304855a1a6fa0f689479a84205e0d4f9e
Afiq-DS <afiq.safri@digitalsymphony.it>
2026-03-19 15:51:50 +0800

    revert back to public

--- src/lib/apolloClient.js
+++ src/lib/apolloClient.js
@@ -3,9 +3,6 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 const client = new ApolloClient({
   link: new HttpLink({
     uri: process.env.NEXT_PUBLIC_WORDPRESS_GRAPHQL_URL,
-    headers: {
-      "User-Agent": "DigitalSymphonyNextApp/1.0",
-    },
   }),
   cache: new InMemoryCache(),
 });
